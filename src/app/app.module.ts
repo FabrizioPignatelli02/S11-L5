15264b7d8eba7a6f5e149bf04c9f91523838311d
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-12-07 16:33:34 +0100

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Route[] = [
   {
     path: '',
-    component: LoginComponent,
+    component: HomeComponent,
   },
   {
     path: 'signup',
     component: SignupComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
@@ -24,6 +29,7 @@ const routes: Route[] = [
     LoginComponent,
     NavbarComponent,
     SignupComponent,
+    HomeComponent,
   ],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
