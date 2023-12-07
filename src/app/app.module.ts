0b4a4b56fc23c9b83eb0d0dfef593dea60953c86
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-12-07 15:27:28 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
